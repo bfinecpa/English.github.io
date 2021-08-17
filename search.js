67224db6bc76decf01faa1be2551e38e21c43778
bfinecpa1@naver.com <bfinecpa1@naver.com>
2021-08-17 13:11:03 +0900

    all

---
+++ search.js
@@ -0,0 +1,385 @@
+//클릭시 주황색 테두리
+// 썸네일 일때
+var file_description = document.querySelectorAll(".file_description");
+for(let i = 0; i<file_description.length; i++)
+{
+    
+    file_description[i].addEventListener("click",()=>
+    {
+        thumb_border_orange_clear();
+        file_description[i].classList.add("file_description_focus");
+    })
+}
+
+function thumb_border_orange_clear()
+{
+    for(let i = 0; i<file_description.length; i++)
+    {
+        if(file_description[i].classList.contains("file_description_focus")){
+            file_description[i].classList.remove("file_description_focus");
+        }
+    }
+}
+// 리스트형일때
+var list_contents_li = document.querySelectorAll(".list_contents_li");
+for(let i = 0; i<list_contents_li.length; i++)
+{
+    
+    list_contents_li[i].addEventListener("click",()=>
+    {
+        list_border_orange_clear();
+        list_contents_li[i].classList.add("list_contents_li_focus");
+    })
+}
+
+function list_border_orange_clear()
+{
+    for(let i = 0; i<list_contents_li.length; i++)
+    {
+        if(list_contents_li[i].classList.contains("list_contents_li_focus")){
+            list_contents_li[i].classList.remove("list_contents_li_focus");
+        }
+    }
+}
+//검색 방식에 따른 placeholder
+var current_search_mode = "모든 행사";
+function change_ph(x)
+{
+
+    current_start_idx = 0;
+    var value = x.value;
+    current_search_mode = value;
+    try {
+        parent.sunny.set_current_search_sheet(value);
+    } catch (err) {}
+
+    var ph= document.querySelector(".search_text");
+    var search_text = document.getElementsByClassName("search_text");
+    if(value=="period")
+    {
+        search_text[0].classList.add("display_none");
+        search_text[1].classList.remove("display_none")
+    }
+    else if(value =="모든 행사")
+    {
+        search_text[0].classList.remove("display_none");
+        search_text[1].classList.add("display_none")
+        ph=ph.getElementsByTagName("input")[0];
+        ph.placeholder="Click search";
+    }
+    else
+    {
+        search_text[0].classList.remove("display_none");
+        search_text[1].classList.add("display_none")
+        ph=ph.getElementsByTagName("input")[0];
+        ph.placeholder="Enter "+ value + " to search for";
+    }
+    
+    
+}
+//보기 형식 명칭 크기에 따라 사이즈 조절
+
+
+
+
+//보기 형식에 따른 보기 변화
+// 리스트 번호 위치에 맞게 썸네일 순서도 맞춤
+
+function change_view(x)
+{
+    var view_foramt_dropdown = document.querySelector(".view_foramt_dropdown");
+    var value= x.value;
+    var list_contents = document.querySelector(".list_contents");
+    var thumbnail_contents = document.querySelector(".thumbnail_contents");
+    
+    var list_page = document.getElementsByClassName("list_page");
+    var contents_row = document.getElementsByClassName("contents_row");
+    var cur_page;
+    var previous_btn = document.querySelector(".previous_btn");
+    var next_btn = document.querySelector(".next_btn");
+
+    if(value=="썸네일형 보기")
+        {
+            view_foramt_dropdown.style.width="145px";
+            for ( let i = 0 ; i < list_page.length; i++)
+            {
+                if(!list_page[i].classList.contains("display_none")){
+                    cur_page = i;
+                }
+            }
+            var cur_filenume=list_page[cur_page].querySelector(".list_file_num").textContent;
+            cur_filenume = Number(cur_filenume)/3;
+            var changed_num = Math.floor(cur_filenume);
+            if(cur_filenume-changed_num==0)
+            {
+                changed_num = changed_num-1;
+            }
+            for ( let i = 0 ; i < contents_row.length; i++)
+            {
+                if(!contents_row[i].classList.contains("display_none")){
+                    cur_page = i;
+                }
+            }
+            contents_row[cur_page].classList.add("display_none");
+            contents_row[changed_num].classList.remove("display_none");
+            //바뀌고 나서 이전 다음 버튼 조정
+            if(contents_row.length<2)
+            {
+                previous_btn.disabled = true;
+                inactive_opacity(previous_btn);
+                next_btn.disabled = true;
+                inactive_opacity(next_btn);
+            }
+            else if(contents_row.length-1 == changed_num)
+            {
+                previous_btn.disabled = false;
+                active_opacity(previous_btn);
+                next_btn.disabled = true;
+                inactive_opacity(next_btn);
+            }
+            else if(changed_num==0)
+            {
+                previous_btn.disabled = true;
+                inactive_opacity(previous_btn);
+                next_btn.disabled = false;
+                active_opacity(next_btn);
+            }
+            else{
+                previous_btn.disabled = false;
+                active_opacity(previous_btn);
+                next_btn.disabled = false;
+                active_opacity(next_btn);
+            }
+            thumbnail_contents.classList.remove("display_none");
+            list_contents.classList.add("display_none");
+            
+        }
+    else//목록형 보기
+    {
+        view_foramt_dropdown.style.width="120px";
+        for ( let i = 0 ; i < contents_row.length; i++)
+        {
+            if(!contents_row[i].classList.contains("display_none")){
+                cur_page = i;
+            }
+        }
+        var cur_filenume = contents_row[cur_page].querySelector(".file_num").textContent;
+        cur_filenume = Number(cur_filenume)/5;
+        var changed_num = Math.floor(cur_filenume);
+        if(cur_filenume-changed_num==0)
+        {
+            changed_num = changed_num-1;
+        }
+        for ( let i = 0 ; i < list_page.length; i++)
+        {
+            if(!list_page[i].classList.contains("display_none")){
+                cur_page = i;
+            }
+        }
+        list_page[cur_page].classList.add("display_none");
+        list_page[changed_num].classList.remove("display_none");
+        //바뀌고 나서 이전 다음 버튼 조정
+        if(list_page.length<2)
+        {
+            previous_btn.disabled = true;
+            inactive_opacity(previous_btn);
+            next_btn.disabled = true;
+            inactive_opacity(next_btn);
+        }
+        else if(list_page.length-1 == changed_num)
+        {
+            previous_btn.disabled = false;
+            active_opacity(previous_btn);
+            next_btn.disabled = true;
+            inactive_opacity(next_btn);
+        }
+        else if(changed_num==0)
+        {
+            previous_btn.disabled = true;
+            inactive_opacity(previous_btn);
+            next_btn.disabled = false;
+            active_opacity(next_btn);
+        }
+        else{
+            previous_btn.disabled = false;
+            active_opacity(previous_btn);
+            next_btn.disabled = false;
+            active_opacity(next_btn);
+        }
+        list_contents.classList.remove("display_none");
+        thumbnail_contents.classList.add("display_none");
+    }
+}
+
+//파일 갯수에 따른 숫자 증가
+//썸네일형
+
+//########################################################################
+
+// 년 월 일에 숫자 넣으면 년 월 일 나오게 끔 하기
+
+var search_year_start = document.getElementById("search_year_start");
+var search_month_start = document.getElementById("search_month_start");
+var search_day_start = document.getElementById("search_day_start");
+var search_year_end = document.getElementById("search_year_end");
+var search_month_end = document.getElementById("search_month_end");
+var search_day_end = document.getElementById("search_day_end");
+
+search_year_start.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_month_start.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_day_start.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_year_end.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_month_end.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_day_end.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+
+
+//----------이전 다음 버튼 ---------------------
+
+
+var list_contents = document.querySelector(".list_contents ");
+var thumbnail_contents = document.querySelector(".thumbnail_contents");
+var list_page = document.getElementsByClassName("list_page");
+var contents_row = document.getElementsByClassName("contents_row");
+var cur_page;
+var previous_btn = document.querySelector(".previous_btn");
+var next_btn = document.querySelector(".next_btn");
+
+
+if(list_page.length < 2)
+{
+    previous_btn.disabled = true;
+    inactive_opacity(previous_btn);
+    next_btn.disabled = true;
+    inactive_opacity(next_btn);
+}
+else{
+    //여러개 있을때 기준
+for ( let i = 0 ; i < list_page.length; i++)
+{
+    if(!list_page[i].classList.contains("display_none")){
+        cur_page = i;
+    }
+}
+if(cur_page==0){
+    previous_btn.disabled = true;
+    inactive_opacity(previous_btn);
+}
+}
+
+
+
+
+next_btn.addEventListener("click", () => {
+    if(!(list_contents.classList.contains("display_none")))
+    {
+        for ( let i = 0 ; i < list_page.length; i++)
+        {
+            if(!list_page[i].classList.contains("display_none")){
+                cur_page = i;
+            }
+        }
+        list_page[cur_page].classList.add("display_none");
+        list_page[cur_page+1].classList.remove("display_none");
+        previous_btn.disabled = false;
+        active_opacity(previous_btn);
+        if(cur_page==(list_page.length-2))
+        {
+            next_btn.disabled=true;
+            inactive_opacity(next_btn);
+        }
+    }
+
+    else if(!(thumbnail_contents.classList.contains("display_none")))
+    {
+        for ( let i = 0 ; i < contents_row.length; i++)
+        {
+            if(!contents_row[i].classList.contains("display_none")){
+                cur_page = i;
+            }
+        }
+        contents_row[cur_page].classList.add("display_none");
+        contents_row[cur_page+1].classList.remove("display_none");
+        previous_btn.disabled = false;
+        active_opacity(previous_btn);
+        if(cur_page==(contents_row.length-2))
+        {
+            next_btn.disabled=true;
+            inactive_opacity(next_btn);
+        }
+    }
+
+    
+})
+
+previous_btn.addEventListener("click", () => {
+    if(!(list_contents.classList.contains("display_none")))
+    {
+        for ( let i = 0 ; i < list_page.length; i++)
+        {
+            if(!list_page[i].classList.contains("display_none")){
+                cur_page = i;
+            }
+        }
+
+        list_page[cur_page].classList.add("display_none");
+        list_page[cur_page-1].classList.remove("display_none");
+        next_btn.disabled=false
+        active_opacity(next_btn);
+        if(cur_page==1)
+        {
+            previous_btn.disabled=true;
+            inactive_opacity(previous_btn);
+        }
+    }
+
+    else if(!(thumbnail_contents.classList.contains("display_none")))
+    {
+        for ( let i = 0 ; i < contents_row.length; i++)
+        {
+            if(!contents_row[i].classList.contains("display_none")){
+                cur_page = i;
+            }
+        }
+
+        contents_row[cur_page].classList.add("display_none");
+        contents_row[cur_page-1].classList.remove("display_none");
+        next_btn.disabled=false
+        active_opacity(next_btn);
+        if(cur_page==1)
+        {
+            previous_btn.disabled=true;
+            inactive_opacity(previous_btn);
+        }
+    }
+    
+})
+
+// opacity 변경
+function inactive_opacity(x)
+{
+    x.classList.add("inactive");
+}
+
+function active_opacity(x)
+{
+    x.classList.remove("inactive");
+}
+
---
+++ search_mob.js
@@ -0,0 +1,224 @@
+//클릭시 주황색 테두리
+//썸네일형
+var file_description = document.querySelectorAll(".file_description");
+var file_thumbnail = document.querySelectorAll(".file_thumbnail");
+for(let i = 0; i<file_description.length; i++)
+{
+    
+    file_description[i].addEventListener("click",()=>
+    {
+        thumb_border_orange_clear();
+        file_description[i].classList.add("file_description_focus");
+        file_thumbnail[i].style.width="339px";   
+    })
+}
+
+function thumb_border_orange_clear()
+{
+    for(let i = 0; i<file_description.length; i++)
+    {
+        if(file_description[i].classList.contains("file_description_focus")){
+            file_description[i].classList.remove("file_description_focus");
+            file_thumbnail[i].style.width="341px" 
+        }
+    }
+}
+
+//리스트 형태
+var list_file_report = document.querySelectorAll(".list_file_report");
+for(let i = 0; i<list_file_report.length; i++)
+{
+    
+    list_file_report[i].addEventListener("click",()=>
+    {
+        list_border_orange_clear();
+        list_file_report[i].classList.add("list_contents_li_focus");
+    })
+}
+
+function list_border_orange_clear()
+{
+    for(let i = 0; i<list_file_report.length; i++)
+    {
+        if(list_file_report[i].classList.contains("list_contents_li_focus")){
+            list_file_report[i].classList.remove("list_contents_li_focus"); 
+        }
+    }
+}
+
+
+
+
+
+
+
+
+//보기 형식에 따른 보기 변화
+
+function change_view(x)
+{
+    var value= x.value;
+    var list_contents = document.querySelector(".list_contents");
+    var thumbnail_contents = document.querySelector(".thumbnail_contents");
+    var previous_next = document.querySelector(".previous_next");
+    if(value=="썸네일형 보기")
+        {
+            thumbnail_contents.classList.remove("display_none");
+            previous_next.classList.remove("display_none");
+            list_contents.classList.add("display_none");
+        }
+    else
+    {
+        list_contents.classList.remove("display_none");
+        thumbnail_contents.classList.add("display_none");
+        previous_next.classList.add("display_none");
+    }
+}
+
+//찾기 클릭시 기준 및 입력 나오기, 닫기 클릭시 닫기
+
+var search_off = document.querySelector(".search_off");
+var search_on = document.querySelector(".search_on");
+var view_foramt_dropdown = document.querySelector(".view_foramt_dropdown");
+var search_dropdown = document.querySelector(".search_dropdown");
+var search_text = document.querySelector(".search_text");
+var search_date = document.querySelector(".search_date");
+var close_btn = document.querySelector(".close_btn");
+search_off.addEventListener("click", () => {
+    search_off.classList.add("display_none");
+    view_foramt_dropdown.classList.add("display_none");
+    search_on.classList.remove("display_none");
+    search_dropdown.classList.remove("display_none");
+    
+    close_btn.classList.remove("display_none");
+    if(search_dropdown.value=="period")
+    {
+        search_date.classList.remove("display_none");
+    }
+    else{
+        search_text.classList.remove("display_none");
+    }
+})
+
+close_btn.addEventListener("click", () => {
+    search_off.classList.remove("display_none");
+    view_foramt_dropdown.classList.remove("display_none");
+    search_on.classList.add("display_none");
+    search_dropdown.classList.add("display_none");
+    search_text.classList.add("display_none");
+    search_date.classList.add("display_none");
+    close_btn.classList.add("display_none");
+})
+
+//검색 방식에 따른 placeholder
+
+function change_ph(x)
+{
+    current_start_idx = 0;
+    var value=x.value;
+    var search_text= document.querySelector(".search_text");
+    var search_date = document.querySelector(".search_date");
+    if(value=="period")
+    {
+        search_text.classList.add("display_none");
+        search_date.classList.remove("display_none")
+    }
+    else
+    {
+        search_text.classList.remove("display_none");
+        search_date.classList.add("display_none")
+        search_text=search_text.getElementsByTagName("input")[0];
+        search_text.placeholder="Enter "+ value ;
+    }
+}
+
+
+//// 년 월 일에 숫자 넣으면 년 월 일 나오게 끔 하기
+
+var search_year_start = document.getElementById("search_year_from");
+var search_month_start = document.getElementById("search_month_from");
+var search_day_start = document.getElementById("search_day_from");
+var search_year_end = document.getElementById("search_year_to");
+var search_month_end = document.getElementById("search_month_to");
+var search_day_end = document.getElementById("search_day_to");
+
+search_year_start.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+
+search_month_start.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_day_start.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_year_end.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_month_end.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+search_day_end.oninput= function(){
+    this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+}
+
+
+
+//이전 다음 버튼
+var contents_row = document.getElementsByClassName("contents_row");
+var cur_page;
+var previous_btn = document.querySelector(".previous_btn");
+var next_btn = document.querySelector(".next_btn");
+
+for ( let i = 0 ; i < contents_row.length; i++)
+    {
+        if(!contents_row[i].classList.contains("display_none")){
+            cur_page = i;
+        }
+    }
+    if(cur_page==0){
+        previous_btn.disabled = true;
+    }
+
+
+
+next_btn.addEventListener("click", () => {
+    
+    for ( let i = 0 ; i < contents_row.length; i++)
+    {
+        if(!contents_row[i].classList.contains("display_none")){
+            cur_page = i;
+        }
+    }
+    contents_row[cur_page].classList.add("display_none");
+    contents_row[cur_page+1].classList.remove("display_none");
+    previous_btn.disabled = false;
+    if(cur_page==(contents_row.length-2))
+    {
+        next_btn.disabled=true;
+    }
+    
+})
+
+previous_btn.addEventListener("click", () => {
+    
+    for ( let i = 0 ; i < contents_row.length; i++)
+    {
+        if(!contents_row[i].classList.contains("display_none")){
+            cur_page = i;
+        }
+    }
+    contents_row[cur_page].classList.add("display_none");
+    contents_row[cur_page-1].classList.remove("display_none");
+    next_btn.disabled=false
+    if(cur_page==1)
+    {
+        previous_btn.disabled=true;
+    }
+})
+
